0e8b0f53f6d68eba83a640511b9fa42483d8616b
ninja <jiachenning@gmail.com>
2013-12-17 23:45:07 +0800

    error day

--- MyApplication/src/main/java/com/example/myapplication/pokemonHachedActivity.java
+++ MyApplication/src/main/java/com/example/myapplication/pokemonHachedActivity.java
@@ -1,7 +1,5 @@
 package com.example.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
 
 public class pokemonHachedActivity extends ActionBarActivity {
 
@@ -18,11 +17,21 @@ public class pokemonHachedActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon_hached);
 
+        Button confirm = (Button)findViewById(R.id.buttonConfirm);
+        Button hp = (Button)findViewById(R.id.buttonHp);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 
